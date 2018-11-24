0b5030fa0ccb3fe5661bc0c70b17a8d33efb6ec7
Junghyun Kim <conankunioi@gmail.com>
2018-11-23 22:30:37 -0500

    Create fetch_stock_data.py

---
+++ fetch_stock_data.py
@@ -0,0 +1,19 @@
+from pandas_datareader import data
+import pandas as pd
+import datetime
+import os
+
+def download_csv(symbols, start_date=None, end_date=None, path='data'):
+	if start_date == None:
+		start_date = '2010-01-01'
+	if end_date == None:
+		end_date = datetime.datetime.today().strftime('%Y-%m-%d')
+	for symbol in symbols:
+		df = data.DataReader(symbol, 'yahoo', start_date, end_date)
+		df.to_csv(os.path.join(path, symbol + '.csv'))
+
+if __name__ == "__main__":
+	# Testing purpose
+	symbols = ['AAPL', 'GOOG', 'FB', 'AMZN']
+
+	download_csv(symbols)
